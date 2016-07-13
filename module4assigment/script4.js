20733dad8135cb88d3a26c29bf65d88bc33143e6
LashmiSudha <d.sudha10@gmail.com>
2016-07-13 10:57:28 +0530

    modl4

--- module4assigment/script4.js
+++ module4assigment/script4.js
@@ -1,16 +1,43 @@
-var a=new Array();
+function fun()
+{
+var a=new Array;
 var ch='y';
 var i=0;
-
-
+var st=new String;
  do
  {
- 	a[i]=prompt("enter name");
+ 	a[i] = prompt("enter name");
  	
  	    i++;
- 	 ch= prompt("Want to continue");
+ 	 ch = prompt("Want to continue");
  	    
 }while(ch=='y'||ch=='Y');
- 	
+
+
+
+    for (var i=0; i<a.length; i++)
+    {
+ 	var j = 0;
+ 	if( a[i].length!=0 )
+ 	 {
+ 	if(a[i][j]=='j'|| a[i][j]=='J') 
+ 	{
+ 		st += "Goodbye "+ a[i] + " "; 
+ 	}
+ 
+      else 
+      {
+      	st +=  "HELLO "+ a[i] + " ";
+  	  }
+     }
+	}
+     
+    return st;
+  }
+
+
+
+
+
 
 
